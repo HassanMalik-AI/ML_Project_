f7232d38dd8ad9ee00c013d1b51e5366abf13020
HassanMalik-AI <hassanmalik1437@gmail.com>
2026-07-05 20:41:37 +0500

    hyperparameter tuninig

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import pandas as pd
 import numpy as np 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import mean_squared_error
 
 
@@ -18,21 +19,24 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,params):
     try:
         report = {} 
-
         for i in range(len(list(models.values()))):
             model = list(models.values())[i]
             model_name = list(models.keys())[i]
             
+            para=params[model_name]
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
-            y_train_pred = model.predict(X_train)
+            
             y_test_pred = model.predict(X_test)
-            train_score = r2_score(y_train,y_train_pred)
             test_score = r2_score(y_test,y_test_pred)
             report[model_name] = test_score
 
-            return report
+        return report
     except Exception as e:
         raise CustomException(e,sys)
